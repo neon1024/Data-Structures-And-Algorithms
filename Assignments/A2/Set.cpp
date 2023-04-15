a4b42f77b3b35e9244bbeb0e5a946c233cc45030
neon1024 <robertscumnpu@gmail.com>
2023-04-15 21:35:00 +0300

    .

--- Assignments/A2/Set.cpp
+++ Assignments/A2/Set.cpp
@@ -3,14 +3,18 @@
 
 Set::Set() {
 	//TODO - Implementation
-}
 
+    this->length = 0;
+    this->capacity = 0;
+
+    this->head = nullptr;
+    this->tail = nullptr;
+}
 
 bool Set::add(TElem elem) {
 	//TODO - Implementation
 	return false;
 }
-
 
 bool Set::remove(TElem elem) {
 	//TODO - Implementation
@@ -22,26 +26,18 @@ bool Set::search(TElem elem) const {
 	return false;
 }
 
-
 int Set::size() const {
-	//TODO - Implementation
-	return 0;
+    return this->length;
 }
-
 
 bool Set::isEmpty() const {
-	//TODO - Implementation
-	return false;
+    return this->length == 0;
 }
-
 
 Set::~Set() {
 	//TODO - Implementation
 }
 
-
 SetIterator Set::iterator() const {
 	return SetIterator(*this);
 }
-
-
--- Assignments/A2/Set.h
+++ Assignments/A2/Set.h
@@ -1,48 +1,52 @@
 #pragma once
-//DO NOT INCLUDE SETITERATOR
+// DO NOT INCLUDE SETITERATOR
 
-//DO NOT CHANGE THIS PART
-#define NULL_TELEM -111111
+// DO NOT CHANGE THIS PART
+#define NULL_TELEM (-111111)
 typedef int TElem;
 class SetIterator;
 
 class Set {
-	//DO NOT CHANGE THIS PART
+	// DO NOT CHANGE THIS PART
 	friend class SetIterator;
 
     private:
-		//TODO - Representation
+		// TODO - Representation
+        int length;
+        int capacity;
+
+        struct Node {
+            TElem element;
+            struct Node* next;
+        };
+
+        Node* head;
+        Node* tail;
 
     public:
-        //implicit constructor
+        // implicit constructor
         Set();
 
-        //adds an element to the set
-		//returns true if the element was added, false otherwise (if the element was already in the set and it was not added)
+        // adds an element to the set
+		// returns true if the element was added, false otherwise (if the element was already in the set)
         bool add(TElem e);
 
-        //removes an element from the set
-		//returns true if e was removed, false otherwise
+        // removes an element from the set
+		// returns true if e was removed, false otherwise
         bool remove(TElem e);
 
-        //checks whether an element belongs to the set or not
+        // checks whether an element belongs to the set or not
         bool search(TElem elem) const;
 
-        //returns the number of elements;
+        // returns the number of elements;
         int size() const;
 
-        //check whether the set is empty or not;
+        // check whether the set is empty or not;
         bool isEmpty() const;
 
-        //return an iterator for the set
+        // return an iterator for the set
         SetIterator iterator() const;
 
         // destructor
         ~Set();
-
 };
-
-
-
-
-
