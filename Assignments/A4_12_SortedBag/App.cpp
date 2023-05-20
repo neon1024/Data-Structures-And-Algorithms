afa3aa3cc430144b1414a9ddcd1270e76c7b01ec
neon1024 <robertscumnpu@gmail.com>
2023-05-20 21:07:19 +0300

    .

--- Assignments/A4_12_SortedBag/App.cpp
+++ Assignments/A4_12_SortedBag/App.cpp
@@ -4,7 +4,11 @@
 #include "ShortTest.h"
 #include "ExtendedTest.h"
 
-using namespace std;
+/*
+ * 12. ADT SortedBag using a hashtable with open addressing (double  hashing) in which the elements are stored.
+ * If an element appears multiple times, it will be stored multiple times in the  hashtable.
+ * In the constructor of the iterator create a sorted array of the elements and use that array for iterating.
+ */
 
 int main() {
     std::cout << "Short testing..." << '\n';
--- Assignments/A4_12_SortedBag/SortedBag.h
+++ Assignments/A4_12_SortedBag/SortedBag.h
@@ -1,3 +1,13 @@
+/*
+ * 12. ADT SortedBag using a hashtable with open addressing (double  hashing) in which the elements are stored.
+ * If an element appears multiple times, it will be stored multiple times in the  hashtable.
+ * In the constructor of the iterator create a sorted array of the elements and use that array for iterating.
+ */
+
+/*
+ * TODO don't create other classes
+ */
+
 #pragma once
 //DO NOT INCLUDE SORTEDBAGITERATOR
 
